e6a1d3abacc983dd0e57055541b7f9012147ea08
Yeasaleh <yeasalehofficial2002@gmail.com>
2024-11-14 18:52:12 +0600

    arrow and slider issue resolved

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -5,6 +5,6 @@ export const breadcrumbsBg = withSuspense(
   lazy(() => import("./breadcrumbs.jpg"))
 );
 
-export const Slide_1 = withSuspense(lazy(() => import("./edu_slide_img_1.jpeg")));
-export const Slide_2 = withSuspense(lazy(() => import("./edu_slide_img_2.jpeg")));
-export const Slide_3 = withSuspense(lazy(() => import("./edu_slide_img_3.webp")));
+export const edu_slide_img_1 = withSuspense(lazy(() => import("./edu_slide_img_1.jpeg")));
+export const edu_slide_img_2 = withSuspense(lazy(() => import("./edu_slide_img_2.jpeg")));
+export const edu_slide_img_3 = withSuspense(lazy(() => import("./edu_slide_img_3.webp")));
