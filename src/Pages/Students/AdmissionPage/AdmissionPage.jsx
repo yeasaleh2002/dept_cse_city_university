466d925b903216def19d758b001f68f1976b8a46
rashed <mdrashedbiswas83@gmail.com>
2024-12-02 23:57:30 +0600

    updated

--- src/Pages/Students/AdmissionPage/AdmissionPage.jsx
+++ src/Pages/Students/AdmissionPage/AdmissionPage.jsx
@@ -20,7 +20,6 @@ const AdmissionPage = () => {
   const handleAdd = (e) => {
     e.preventDefault();
     const form = e.target;
-   
     const formData = {
       first_name: form.first_name.value,
       last_name: form.last_name.value,
@@ -29,7 +28,7 @@ const AdmissionPage = () => {
       date_of_birth: form.date_of_birth.value,
       address: form.address.value,
       gender: form.gender.value,
-      photo: form.photo.files[0],
+      // photo: form.photo.files[0],
       father_name: form.father_name.value,
       mother_name: form.mother_name.value,
       batch: form.batch.value,
@@ -68,6 +67,8 @@ const AdmissionPage = () => {
         console.error("Error submitting form:", error);
         alert("Failed to submit the form. Please try again.");
       });
+    
+
   };
   
 
@@ -150,7 +151,7 @@ const AdmissionPage = () => {
                     <MenuItem value="Other">Other</MenuItem>
                   </TextField>
                 </Grid>
-                <Grid item xs={12}>
+                {/* <Grid item xs={12}>
                   <Box
                     sx={{
                       display: "flex",
@@ -175,7 +176,7 @@ const AdmissionPage = () => {
                       <input type="file" name="photo" />
                     </Button>
                   </Box>
-                </Grid>
+                </Grid> */}
               </Grid>
 
               <Typography variant="h6" mt={4} mb={2}>
